2ca9c9c4518e42d4ce05447a78813ebe86a81940
iulian0585 <iam.constantinescu@gmail.com>
2023-10-19 14:35:30 +0300

    sandbox s3 bucket reconfigure

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -2,5 +2,8 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Sandbox"
+  }
 }
